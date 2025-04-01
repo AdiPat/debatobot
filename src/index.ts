6b543de087f8524a6a31fdecfc4da13eb1df1485
Aditya Patange <contact.adityapatange@gmail.com>
2025-04-01 05:30:46 +0530

    feat: add basic chat loop on startup script

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
+import { ChatLoop } from "./core/chat-loop";
+import { naiveChatMessageHandler } from "./core/chat-handlers";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env",
+  override: true,
+});
+
 /**
  * Main function to run the SynthLite CLI.
  */
 export async function main() {
+  const chatLoop = new ChatLoop(naiveChatMessageHandler);
   console.log("Welcome to DebatoBot!");
+  console.log("Type 'q' or 'Q' to quit.");
+  await chatLoop.start();
 }
